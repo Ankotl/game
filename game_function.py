014a2014f5a48f5d30a3f3ef35f442571d06b2e7
anton_kotl <llluzuk@mail.ru>
2020-04-11 15:08:57 +0300

    add bullet

--- game_function.py
+++ game_function.py
@@ -1,8 +1,9 @@
 import sys
 import pygame
+from bullet import Bullet
 
 
-def check_keydown_events(event, ship):
+def check_keydown_events(event, game_set, screen, ship, bullets):
     if event.key == pygame.K_RIGHT:
         ship.moving_right = True
     elif event.key == pygame.K_LEFT:
@@ -11,6 +12,10 @@ def check_keydown_events(event, ship):
         ship.moving_up = True
     elif event.key == pygame.K_DOWN:
         ship.moving_down = True
+    elif event.key == pygame.K_SPACE:
+        if len(bullets) < game_set.bullets_allowed:
+            new_bullet = Bullet(game_set, screen, ship)
+            bullets.add(new_bullet)
 
 
 def check_keyup_events(event, ship):
@@ -24,17 +29,25 @@ def check_keyup_events(event, ship):
         ship.moving_down = False
 
 
-def check_events(ship):
+def check_events(game_set, screen, ship, bullets):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            check_keydown_events(event, ship)
+            check_keydown_events(event, game_set, screen, ship, bullets)
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
 
 
-def update_screen(game_set, screen, ship):
+def update_bullet(bullets):
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
+
+
+def update_screen(game_set, screen, ship, bullets):
     screen.fill(game_set.bg_color)
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
     ship.blitme()
     pygame.display.flip()
