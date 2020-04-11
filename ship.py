d91d2a2dfc3909e0778e68a3359ab7892fbd220a
anton_kotl <llluzuk@mail.ru>
2020-04-11 14:38:54 +0300

    add move up and down

--- ship.py
+++ ship.py
@@ -13,12 +13,18 @@ class Ship():
         self.rect.bottom = self.screen_rect.bottom
         self.moving_right = False
         self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
 
     def update(self):
         if self.moving_right and self.rect.right < self.screen_rect.right:
             self.center += self.game_set.ship_speed_factor
         if self.moving_left and self.rect.left > 0:
             self.center -= self.game_set.ship_speed_factor
+        if self.moving_up and self.rect.bottom > self.screen_rect.bottom/2:
+            self.rect.bottom -= self.game_set.ship_speed_factor
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.rect.bottom += self.game_set.ship_speed_factor
         self.rect.centerx = self.center
 
     def blitme(self):
